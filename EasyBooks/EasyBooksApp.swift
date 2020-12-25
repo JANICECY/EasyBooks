628ac1c92afb0cdbd0f22c475a0e60f580be9b24
Yiyao Zhang <me@yiyao.ca>
2020-12-25 15:50:48 -0500

    Initial Commit

---
+++ EasyBooks/EasyBooksApp.swift
@@ -0,0 +1,20 @@
+//
+//  EasyBooksApp.swift
+//  EasyBooks
+//
+//  Created by Yiyao Zhang on 2020-12-25.
+//
+
+import SwiftUI
+
+@main
+struct EasyBooksApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
